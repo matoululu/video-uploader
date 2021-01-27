cd0ebe7caec2e75b75bc7b7b3c0f5cc0e8ef75d7
Matt <matthew.marineau@gmail.com>
2021-01-27 13:50:20 -0500

    ios

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -112,7 +112,7 @@ class Recorder extends React.Component {
       <div className="video__wrapper">
         <div className="video__blurb">
           <p>Welcome to Emily's 29th birthday surprise! I've built this site to gather a collection of warm birthday wishes to piece together for Emily, this way we won't all feel so separated from one another if even for a short moment.</p>
-          <p className="ios">It appears you may be using an iPhone! Unfortunately, iOS doesn't support the features used here. Your best bet is to use a laptop, desktop or android device. Alternatively, you can enable this experimental feature by going to 'Settings' > 'Safari' > 'Advanced' > 'Experimental features' > enabled 'MediaRecorder' and then reload the page.</p>
+          <p className="ios">It appears you may be using an iPhone! Unfortunately, iOS doesn't support the features used here. Your best bet is to use a laptop, desktop or android device. Alternatively, you can enable this experimental feature by going to 'Settings' > 'Safari' > 'Advanced' > 'Experimental features' > enable 'MediaRecorder' and than reload the page.</p>
         </div>
         <div className="video__content">
           <video className="video video-js vjs-default-skin" playsInline id="video"  ></video>
